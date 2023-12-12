84ff9737a4b4a8ee545079e715aebb9cdae823d7
mwangiowen <owen31588@gmail.com>
2023-12-12 09:54:44 +0300

    update 1 file and delete 1 file

--- Lib/Migration.py
+++ Lib/Migration.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from model import Base, Restaurant, Customer, Review
 
 
-engine = create_engine('sqlite:///restaurant_reviews.db')
+engine = create_engine('sqlite:///restaurant.db')
 
 
 Base.metadata.bind = engine
